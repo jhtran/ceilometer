0f2511476b70fc7e599bf5d6ed5f8a288b4956a1
John H. Tran <jhtran@att.com>
2012-08-31 11:17:43 -0700

    implement sqlalchemy dbengine backend
    implements bug #1021767
    
    To sync db migration, for now use temporary
    script tools/dbsync.  Modify it to point
    to your db url (example 'sqlite:///ceilometer.db'
    or 'mysql://user:mypass@localhost/ceilometer').
    Also ensure the database_connection= in conf
    files point to same db url.
    
    Change-Id: I1c72b0a6b860738e94cf19666cb47db88ac9b251

---
+++ ceilometer/storage/sqlalchemy/migrate_repo/versions/001_add_meter_table.py
@@ -0,0 +1,70 @@
+from sqlalchemy import *
+from ceilometer.openstack.common import timeutils
+
+meta = MetaData()
+
+meter = Table(
+    'meter', meta,
+    Column('id', Integer, primary_key=True),
+    Column('counter_name', String(255)),
+    Column('user_id', String(255)),
+    Column('project_id', String(255)),
+    Column('resource_id', String(255)),
+    Column('resource_metadata', String(5000)),
+    Column('counter_type', String(255)),
+    Column('counter_volume', Integer),
+    Column('counter_duration', Integer),
+    Column('timestamp', DateTime(timezone=False)),
+    Column('message_signature', String(1000)),
+    Column('message_id', String(1000))
+)
+
+resource = Table(
+    'resource', meta,
+    Column('id', String(255), primary_key=True),
+    Column('resource_metadata', String(5000)),
+    Column('project_id', String(255)),
+    Column('received_timestamp', DateTime(timezone=False)),
+    Column('timestamp', DateTime(timezone=False)),
+    Column('user_id', String(255))
+)
+
+user = Table(
+    'user', meta,
+    Column('id', String(255), primary_key=True),
+)
+
+project = Table(
+    'project', meta,
+    Column('id', String(255), primary_key=True),
+)
+
+sourceassoc = Table(
+    'sourceassoc', meta,
+    Column('source_id', String(255)),
+    Column('user_id', String(255)),
+    Column('project_id', String(255)),
+    Column('resource_id', String(255)),
+    Column('meter_id', Integer)
+)
+
+source = Table(
+    'source', meta,
+    Column('id', String(255), primary_key=True),
+    UniqueConstraint('id')
+)
+
+
+tables = [meter, project, resource, user, source, sourceassoc]
+
+
+def upgrade(migrate_engine):
+    meta.bind = migrate_engine
+    for i in tables:
+        i.create()
+
+
+def downgrade(migrate_engine):
+    meta.bind = migrate_engine
+    for i in tables:
+        i.drop()
---
+++ ceilometer/storage/sqlalchemy/models.py
@@ -0,0 +1,175 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright (c) 2011 X.commerce, a business unit of eBay Inc.
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# Copyright 2011 Piston Cloud Computing, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""
+SQLAlchemy models for nova data.
+"""
+
+import json
+from sqlalchemy import Column, Integer, BigInteger, String, schema, Table
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey, DateTime, Boolean, Text, Float
+from sqlalchemy.orm import relationship, backref, object_mapper
+from sqlalchemy.types import TypeDecorator, VARCHAR
+
+from ceilometer.storage.sqlalchemy.session import get_session
+from ceilometer.openstack.common import timeutils
+
+
+class JSONEncodedDict(TypeDecorator):
+    "Represents an immutable structure as a json-encoded string."
+
+    impl = VARCHAR
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = json.loads(value)
+        return value
+
+
+class CeilometerBase(object):
+    """Base class for Ceilometer Models."""
+    __table_args__ = {'mysql_engine': 'InnoDB'}
+    __table_initialized__ = False
+
+    def save(self, session=None):
+        """Save this object."""
+        if not session:
+            session = get_session()
+        session.add(self)
+        try:
+            session.flush()
+        except Exception:
+            raise
+
+    def delete(self, session=None):
+        """Delete this object."""
+        self.save(session=session)
+
+    def __setitem__(self, key, value):
+        setattr(self, key, value)
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+    def get(self, key, default=None):
+        return getattr(self, key, default)
+
+    def __iter__(self):
+        columns = dict(object_mapper(self).columns).keys()
+        # NOTE(russellb): Allow models to specify other keys that can be looked
+        # up, beyond the actual db columns.  An example would be the 'name'
+        # property for an Instance.
+        if hasattr(self, '_extra_keys'):
+            columns.extend(self._extra_keys())
+        self._i = iter(columns)
+        return self
+
+    def next(self):
+        n = self._i.next()
+        return n, getattr(self, n)
+
+    def update(self, values):
+        """Make the model object behave like a dict"""
+        for k, v in values.iteritems():
+            setattr(self, k, v)
+
+    def iteritems(self):
+        """Make the model object behave like a dict.
+
+        Includes attributes from joins."""
+        local = dict(self)
+        joined = dict([(k, v) for k, v in self.__dict__.iteritems()
+                      if not k[0] == '_'])
+        local.update(joined)
+        return local.iteritems()
+
+
+Base = declarative_base(cls=CeilometerBase)
+
+
+sourceassoc = Table('sourceassoc', Base.metadata,
+    Column('meter_id', Integer, ForeignKey("meter.id")),
+    Column('project_id', String(255), ForeignKey("project.id")),
+    Column('resource_id', String(255), ForeignKey("resource.id")),
+    Column('user_id', String(255), ForeignKey("user.id")),
+    Column('source_id', String(255), ForeignKey("source.id"))
+)
+
+
+class Source(Base):
+    __tablename__ = 'source'
+    id = Column(String(255), primary_key=True)
+
+
+class Meter(Base):
+    """Metering data"""
+
+    __tablename__ = 'meter'
+    id = Column(Integer, primary_key=True)
+    counter_name = Column(String(255))
+    sources = relationship("Source", secondary=lambda: sourceassoc,
+                           lazy='joined')
+    user_id = Column(String(255), ForeignKey('user.id'))
+    project_id = Column(String(255), ForeignKey('project.id'))
+    resource_id = Column(String(255), ForeignKey('resource.id'))
+    resource_metadata = Column(JSONEncodedDict)
+    counter_type = Column(String(255))
+    counter_volume = Column(Integer)
+    counter_duration = Column(Integer)
+    timestamp = Column(DateTime, default=timeutils.utcnow)
+    message_signature = Column(String)
+    message_id = Column(String)
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(String(255), primary_key=True)
+    sources = relationship("Source", secondary=lambda: sourceassoc,
+                           lazy='joined')
+    resources = relationship("Resource", backref='user', lazy='joined')
+    meters = relationship("Meter", backref='user', lazy='joined')
+
+
+class Project(Base):
+    __tablename__ = 'project'
+    id = Column(String(255), primary_key=True)
+    sources = relationship("Source", secondary=lambda: sourceassoc,
+                           lazy='joined')
+    resources = relationship("Resource", backref='project', lazy='joined')
+    meters = relationship("Meter", backref='project', lazy='joined')
+
+
+class Resource(Base):
+    __tablename__ = 'resource'
+    id = Column(String(255), primary_key=True)
+    sources = relationship("Source", secondary=lambda: sourceassoc,
+                           lazy='joined')
+    timestamp = Column(DateTime)
+    resource_metadata = Column(JSONEncodedDict)
+    received_timestamp = Column(DateTime, default=timeutils.utcnow)
+    user_id = Column(String(255), ForeignKey('user.id'))
+    project_id = Column(String(255), ForeignKey('project.id'))
+    meters = relationship("Meter", backref='resource', lazy='joined')
