12d1b628d8c9913de7df3e44a6aa0b799a86133b
John Tran <jhtran@att.com>
2012-09-19 19:30:13 +0000

    added table index, and ordered migration upgrade / reverse downgrade
    Change-Id: I5e8d61a5852d8fd27fead94561439da9a6ac290e

--- ceilometer/storage/sqlalchemy/migrate_repo/versions/001_add_meter_table.py
+++ ceilometer/storage/sqlalchemy/migrate_repo/versions/001_add_meter_table.py
@@ -5,52 +5,56 @@ meta = MetaData()
 
 meter = Table(
     'meter', meta,
-    Column('id', Integer, primary_key=True),
+    Column('id', Integer, primary_key=True, index=True),
     Column('counter_name', String(255)),
-    Column('user_id', String(255)),
-    Column('project_id', String(255)),
+    Column('user_id', String(255), index=True),
+    Column('project_id', String(255), index=True),
     Column('resource_id', String(255)),
     Column('resource_metadata', String(5000)),
     Column('counter_type', String(255)),
     Column('counter_volume', Integer),
     Column('counter_duration', Integer),
-    Column('timestamp', DateTime(timezone=False)),
+    Column('timestamp', DateTime(timezone=False), index=True),
     Column('message_signature', String(1000)),
     Column('message_id', String(1000))
 )
 
 resource = Table(
     'resource', meta,
-    Column('id', String(255), primary_key=True),
+    Column('id', String(255), primary_key=True, index=True),
     Column('resource_metadata', String(5000)),
-    Column('project_id', String(255)),
+    Column('project_id', String(255), index=True),
     Column('received_timestamp', DateTime(timezone=False)),
-    Column('timestamp', DateTime(timezone=False)),
-    Column('user_id', String(255))
+    Column('timestamp', DateTime(timezone=False), index=True),
+    Column('user_id', String(255), index=True)
 )
 
 user = Table(
     'user', meta,
-    Column('id', String(255), primary_key=True),
+    Column('id', String(255), primary_key=True, index=True),
 )
 
 project = Table(
     'project', meta,
-    Column('id', String(255), primary_key=True),
+    Column('id', String(255), primary_key=True, index=True),
 )
 
 sourceassoc = Table(
     'sourceassoc', meta,
-    Column('source_id', String(255)),
+    Column('source_id', String(255), index=True),
     Column('user_id', String(255)),
     Column('project_id', String(255)),
     Column('resource_id', String(255)),
-    Column('meter_id', Integer)
+    Column('meter_id', Integer),
+    Index('idx_su', 'source_id', 'user_id'),
+    Index('idx_sp', 'source_id', 'project_id'),
+    Index('idx_sr', 'source_id', 'resource_id'),
+    Index('idx_sm', 'source_id', 'meter_id')
 )
 
 source = Table(
     'source', meta,
-    Column('id', String(255), primary_key=True),
+    Column('id', String(255), primary_key=True, index=True),
     UniqueConstraint('id')
 )
 
@@ -60,11 +64,11 @@ tables = [meter, project, resource, user, source, sourceassoc]
 
 def upgrade(migrate_engine):
     meta.bind = migrate_engine
-    for i in tables:
+    for i in sorted(tables):
         i.create()
 
 
 def downgrade(migrate_engine):
     meta.bind = migrate_engine
-    for i in tables:
+    for i in sorted(tables, reverse=True):
         i.drop()
